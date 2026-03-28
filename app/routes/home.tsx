d316f917f6758e30936af0e84cd0eed538b53ac4
Jack-Muoio <jackmuoio81@gmail.com>
2026-03-28 12:04:09 -0400

    implement drag and drop uploader

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,8 @@ import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
 import { ArrowRight, Clock } from "lucide-react";
 import Button from "../../components/ui/Button";
+import Upload from "../../components/Upload";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +13,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const navigate = useNavigate()
+
+  const handleUploadComplete = async (base64Image: string) => {
+    const newId = Date.now().toString();
+
+    navigate(`/visualizer/${newId}`);
+
+    return true;
+  }
   return (
       <div className="home">
         <Navbar />
@@ -19,7 +30,7 @@ export default function Home() {
             <div className="dot">
               <div className="pulse"/>
             </div>
-            <p>Intoroducing Roomify 2.0</p>
+            <p>Introducing Roomify 2.0</p>
           </div>
           <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
           <p className="subtitle">Roomify is an AI-first design environment that helps you visualize,
@@ -40,7 +51,7 @@ export default function Home() {
                 <h3>Upload your floor plan</h3>
                 <p>Supports JPG, PNG, formats up to 10MB</p>
               </div>
-              <p>Upload images</p>
+              <Upload onComplete={handleUploadComplete}/>
             </div>
           </div>
         </section>
